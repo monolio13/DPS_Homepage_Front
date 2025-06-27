26151460950e7498c34c7a23180e8be685e254a1
goodfriends413 <goodfriends413@gmail.com>
2025-06-27 19:49:05 +0900

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @format */
 
 import type { NextConfig } from "next";
-import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,21 +8,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config: Configuration, { isServer }) => {
-    if (!isServer) {
-      config.watchOptions = {
-        ignored: [
-          '**/node_modules/**',
-          '**/.git/**',
-          '**/.next/**',
-          '**/dist/**',
-          '**/logs/**',
-          '**/output/**',
-        ],
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
+
